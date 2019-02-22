e3a514df6597b9192528ad987e8a59f15df8b089
cfcrotty <carafeliselana@gmail.com>
2019-02-21 19:09:07 -0800

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,15 +15,24 @@ var computerText = document.getElementById("computerChoice");
 // Creates an array that lists alphabet.
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//This function resets counter for "Guesses Left", "User Guess", resetCC, guessLeftScore, hidden computer choice
+function resetAll() {
+    guessLeftText.textContent = 0;
+    resetCC = 1;
+    userText.textContent = "";
+    computerText.textContent = "";
+    guessLeftScore = 9;
+}
+
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
     // Determines which key was pressed.
     var userGuess = event.key;
-    userText.textContent += userGuess+",";
+    
     if (resetCC) {
         // Randomly chooses a choice from the options array. This is the Computer's guess.
         computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        //computerText.textContent = computerGuess;
+        computerText.textContent = computerGuess;
         resetCC=0;
     }
     userGuess = userGuess.toLowerCase();
@@ -31,25 +40,20 @@ document.onkeyup = function (event) {
 
     if (chk < 0) {
         alert("Please enter letters from a/A-z/Z only.");
-    } else if (computerGuess === userGuess) {
-        winScore++;
-        winsTally.textContent = winScore;
-        guessLeftText.textContent = 0;
-        resetCC = 1;
-        userText.textContent = "";
-        computerText.textContent = "";
-        guessLeftScore = 9;
     } else {
-        guessLeftScore--;
-        guessLeftText.textContent = guessLeftScore;
-        if (guessLeftScore==0) {
-            lossesScore++;
-            lossesTally.textContent = lossesScore;
-            guessLeftText.textContent = 0;
-            resetCC = 1;
-            userText.textContent = "";
-            computerText.textContent = "";
-            guessLeftScore = 9;
+        userText.textContent += userGuess+",";
+        if (computerGuess === userGuess) {
+            winScore++;
+            winsTally.textContent = winScore;
+            resetAll();
+        } else {
+            guessLeftScore--;
+            guessLeftText.textContent = guessLeftScore;
+            if (guessLeftScore==0) {
+                lossesScore++;
+                lossesTally.textContent = lossesScore;
+                resetAll();
+            }
         }
     }
     /*
